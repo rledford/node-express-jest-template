c86c5b30f972009b5c14a8acf6af801b84be877f
rledford <ryan.c.ledford@gmail.com>
2021-08-01 09:17:49 -0500

    added cors to api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const cors = require('cors');
 const router = require('express').Router();
 
+router.use(cors());
 router.use('/users', require('./users'));
 
 module.exports = router;
